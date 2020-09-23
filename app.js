211828041a4891094d8863a9f3c1c1ed598bcccf
joao pedro <fonsecajope@gmail.com>
2020-09-23 16:57:35 -0300

    Modify deleteCheck, add filterTodo, and declare filterOption

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 //Event Listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
 // Functions
 function addTodo(event) {
@@ -38,8 +40,35 @@ function deleteCheck(e) {
 	const item = e.target;
 	const todo = item.parentElement;
 	if (item.classList[0] === "trash-button") {
-		todo.remove();
-	} else if (item.classList[0] === "check-button") {
+		todo.classList.add("fall");
+		todo.addEventListener("transitionend", function () {
+			todo.remove();
+		});
+	} else if (item.classList[0] === "complete-button") {
 		todo.classList.toggle("completed");
 	}
+}
+
+function filterTodo(e) {
+	const todos = todoList.childNodes;
+	todos.forEach(function (todo) {
+		switch (e.target.value) {
+			case "all":
+				break;
+			case "completed":
+				if (todo.classList.contains("completed")) {
+					todo.style.display = "flex";
+				} else {
+					todo.style.display = "none";
+				}
+				break;
+			case "uncompleted":
+				if (!todo.classList.contains("completed")) {
+					todo.style.display = "flex";
+				} else {
+					todo.style.display = "none";
+				}
+				break;
+		}
+	});
 }
